8b8df121ce90ea5d252031d8275df9f0fe622550
Daniel Xing <danielxing97@gmail.com>
2018-05-01 21:05:21 -0400

    case fix

--- test.c
+++ test.c
@@ -9,14 +9,13 @@ int *lookup(int next, int current, int carry) {
         lookup_result[0] = 0;
         lookup_result[1] = 0;
         switch(compound) {
-                case 4:
-                case 6:
+                case 3:
                 case 7:
-                case 8:
                         lookup_result[0] = 1;
                 case 5:
                 case 6:
                         lookup_result[1] = -1;
+                        lookup_result[0] = 1;
                         break;
                 case 1:
                 case 2:
