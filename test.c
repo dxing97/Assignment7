8d616ab1586e9278896f9cd3df9e7a828d979e8c
Daniel Xing <danielxing97@gmail.com>
2018-05-01 21:01:10 -0400

    initial code commit

---
+++ test.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#define INPUT 0x2BEF
+#define INPUT2 0xBDAB
+
+int lookup_result[2]; //0 - carry, 1 - recoded
+
+int *lookup(int next, int current, int carry) {
+        int compund = next * 4 + current * 2 + carry;
+        lookup_result[0] = 0;
+        lookup_result[1] = 0;
+        switch(compound) {
+                case 4:
+                case 6:
+                case 7:
+                case 8:
+                        lookup_result[0] = 1;
+                case 5:
+                case 6:
+                        lookup_result[1] = -1;
+                        break;
+                case 1:
+                case 2:
+                        lookup_result[1] = 1;
+                        break;
+        }
+        return lookup_result;
+}
+
+int main() {
+        /*
+        read in the input char by char
+        first carry in is 0
+        match current bit and carry in with table
+        output recided bit and carryout
+        */
+        int input = INPUT, output = 0;
+        int carry = 0, current= input & 1, next = input & 2;
+        int *lookup_results;
+        int output[32];
+        int currentdigit = 0;
+        while (input != 0) {
+                current = input & 1;
+                next = input & 2;
+                lookup_results = lookup(next, current, carry);
+                carry = lookup_results[0];
+                output[currentdigit] = lookup_results[1];
+                currentdigit++;
+                input = input >> 1;
+        }
+        int i;
+        for(i = 0; i < 32; i++) {
+                printf("%d, ", output[i]);
+        }
+        return 0;
+}
+        
+        
